71d8b5d029d10c0b7f93a8fa002b18be44dbaeb0
uruguaj <leo.potocky@protonmail.com>
2023-12-15 15:34:42 +0100

    book details , list

--- bookrController/reviews/models.py
+++ bookrController/reviews/models.py
@@ -1,23 +1,20 @@
-from django.db import models
 from django.contrib import auth
+from django.db import models
 
-
-# Create your models here.
 
 class Publisher(models.Model):
-    name = models.CharField(
-        max_length=50,
-        help_text="The name of the Publisher."
-    )
-    website = models.URLField(
-        help_text="The Publisher website."
-    )
-    email = models.EmailField(
-        help_text="Publisher email."
-    )
+    """A company that publishes books."""
+    name = models.CharField(max_length=50,
+                            help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The Publisher's website.")
+    email = models.EmailField(help_text="The Publisher's email address.")
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
+    """A published book."""
     title = models.CharField(max_length=70,
                              help_text="The title of the book.")
     publication_date = models.DateField(
@@ -34,6 +31,7 @@ class Book(models.Model):
 
 
 class Contributor(models.Model):
+    """A contributor to a Book, e.g. author, editor, co-author."""
     first_names = models.CharField(max_length=50,
                                    help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50,
@@ -66,3 +64,4 @@ class Review(models.Model):
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE,
                              help_text="The Book that this review is for.")
+
