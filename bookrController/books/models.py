29de46a481e81b088d19b41a86a1033ad7632f69
uruguaj <leo.potocky@protonmail.com>
2024-01-05 20:12:12 +0100

    neviem fr fr

--- bookrController/books/models.py
+++ bookrController/books/models.py
@@ -5,6 +5,9 @@ class Book(models.Model):
     subtitle = models.CharField(max_length=250)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
+    image = models.ImageField(upload_to="image", default='templates/books/IMG_2181.jpg')
+
+
 
     def __str__(self) -> str:
         return self.title
