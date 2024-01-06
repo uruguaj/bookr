eea3ed9da8320d8831cda8790f9e1a0c608ea31e
uruguaj <leo.potocky@protonmail.com>
2024-01-06 21:43:51 +0100

    B0SS

--- bookrController/books/models.py
+++ bookrController/books/models.py
@@ -4,7 +4,8 @@ class Publication(models.Model):
     title = models.CharField(max_length=250)
     subtitle = models.CharField(max_length=250)
     author = models.CharField(max_length=100)
-    isbn = models.CharField(max_length=13)
+    isbn = models.IntegerField()
+    imagePath = models.CharField(max_length=1000, default="https://uruguaj.com/nn.jpeg")
 
     class Meta:
         abstract = True
