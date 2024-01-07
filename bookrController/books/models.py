3c62848fcf74465a5e37dab7f62e221d636bc904
uruguaj <leo.potocky@protonmail.com>
2024-01-07 12:35:31 +0100

    animations, design , Regex controller

--- bookrController/books/models.py
+++ bookrController/books/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
 
 class Publication(models.Model):
     title = models.CharField(max_length=250)
     subtitle = models.CharField(max_length=250)
     author = models.CharField(max_length=100)
-    isbn = models.IntegerField()
+    isbn = models.CharField(max_length=25, validators=[RegexValidator(r'^\d{1,25}$', 'ISBN must contain only numbers.')])
     imagePath = models.CharField(max_length=1000, default="https://uruguaj.com/nn.jpeg")
 
     class Meta:
